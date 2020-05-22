d1dd1737990fd3206a2c09496f77d2b06ab996a1
vijit malik <vijit.malik.pinna.vm@gmail.com>
2020-05-22 18:00:12 +0530

    components part 1

--- src/App.js
+++ src/App.js
@@ -2,16 +2,27 @@ import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { Navbar, NavbarBrand} from 'reactstrap';
-class App extends Component{
-  render(){
-    return (
-     <div className="App">
-        <Navbar dark color="primary">
-          <div className="container" >
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
+import  Menu from './components/MenuComponent';
 
+
+
+class App extends Component{
+
+  constructor(props){
+      super(props);
+
+  }
+  render(){
+
+    return (
+       <div className="App">
+          <Navbar dark color="primary">
+            <div className="container" >
+              <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+            </div>
+          </Navbar>
+
+          <Menu/>
      </div>
     );
   }
